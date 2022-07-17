92fa0aa1c130670a4f71a8125398033c170d7254
Blandfish <Ankruse10@ksu.edu>
2022-07-16 21:09:28 -0500

    Search functions in controller. still need to rewrite the GUI and link it to them

--- Controller.py
+++ Controller.py
@@ -1,4 +1,35 @@
+import requests
+import urllib
+from requests_html import HTML
+from requests_html import HTMLSession
+
 class MainController:
     def __init__(self, model, view):
         self.model = model
         self.view = view
+
+    def getResponse(query):
+        try:
+            session = HTMLSession()
+            response = session.get(query)
+            return response
+        except requests.exceptions.RequestException as e:
+            print(e)
+            
+    def get_search(self, query):
+        query = urllib.parse.quote_plus(query)
+        response = getResponse("https://www.google.com/search?q="
+                              + query + "reviews")
+        links = list(response.html.absolute_links)
+
+        target = 'http://www.imdb.com/title'
+
+        for url in links[:]:
+            if not (url.startswith(target)) or "reviews" not in url:
+                links.remove(url)
+            #need to prompt user through view if there are 
+            #still multiple links at this point
+
+        return links
+
+
